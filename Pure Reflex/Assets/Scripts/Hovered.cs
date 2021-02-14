140429ea0fc500320d845db1c21eb1ac0bfb09b5
Oliver Van Dyk <n10474382@qut.edu.au>
2021-02-14 10:13:30 +1000

    Hovering Added
    Asset Store:
    Quick Outline
    
    Code:
    Hovered Script Added
     - Hovering over a GameObject labelled Player will highlight the object Dynamically (Blue or Red)

---
+++ Pure Reflex/Assets/Scripts/Hovered.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hovered : MonoBehaviour
+{
+    private Outline outlineScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outlineScript = GetComponent<Outline>();
+    }
+
+    void OnHitEnter(GameObject Player)
+    {   
+        if (gameObject.Equals(Player))
+            outlineScript.OutlineColor = Color.blue;
+        else
+            outlineScript.OutlineColor = Color.red;
+        outlineScript.OutlineWidth = 3;
+    }
+
+    void OnHitExit()
+    {   
+        outlineScript.OutlineColor = Color.white;
+        outlineScript.OutlineWidth = 0;
+    }
+}
--- Pure Reflex/Assets/Scripts/InputTargeting.cs
+++ Pure Reflex/Assets/Scripts/InputTargeting.cs
@@ -9,18 +9,23 @@ public class InputTargeting : MonoBehaviour
     RaycastHit hit;
 
     void Start()
-    {  
+    {
         // Find player gameobject on Parent
         selectedHero = transform.parent.Find("Player").gameObject;
     }
 
     void Update()
     {
+        Selected();
+        Hover();
+    }
+    void Selected()
+    {
         // Player Targeting
         if (Input.GetMouseButtonDown(1))
         {
-            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
-            {   
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
                 // IF the player is targetable
                 if (hit.collider.GetComponent<Targetable>() != null)
                 {
@@ -28,15 +33,61 @@ public class InputTargeting : MonoBehaviour
                     {
                         selectedHero.GetComponent<HeroCombat>().targetEnemy = hit.collider.gameObject;
                         Debug.Log("Enemy Located at: " + hit.collider.transform.position);
+
                     }
 
-                    else if(hit.collider.GetComponent<Targetable>() == null)
+                    else if (hit.collider.GetComponent<Targetable>() == null)
                     {
                         selectedHero.GetComponent<HeroCombat>().targetEnemy = null;
-                    
+
                     }
                 }
             }
         }
+    }
+    private bool hitting = false;
+    private GameObject hitObject;
+    void Hover()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+        {
+
+            GameObject go = hit.transform.gameObject;
+
+            // IF the player is targetable
+            if (hit.collider.GetComponent<Targetable>() != null)
+            {
+                // If no registred hitobject => Entering
+                if (hitObject == null)
+                {
+                    go.SendMessage("OnHitEnter", selectedHero);
+                }
+                // If hit object is the same as the registered one => Stay
+                else if (hitObject.GetInstanceID() == go.GetInstanceID())
+                {
+                    //hitObject.SendMessage( "OnHitStay" );
+                }
+                // If new object hit => Exit last + Enter new
+                else
+                {
+                    hitObject.SendMessage("OnHitExit");
+                    //go.SendMessage("OnHitEnter");
+                }
+
+                hitting = true;
+                hitObject = go;
+            }
+
+            // No object hit => Exit last one
+            else if (hitting)
+            {
+                hitObject.SendMessage("OnHitExit");
+                hitting = false;
+                hitObject = null;
+            }
+            //hit.transform.SendMessage("Hovering");
+
+        }
+
     }
 }
